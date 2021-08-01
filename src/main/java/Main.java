1d7d97be6cae01c099f8b876685fac157df25000
davidbi <davidbi@nice.com>
2021-08-01 17:44:29 +0300

    //

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,26 +12,7 @@ public class Main {
         System.out.println("Welcome to currency converter");
         Runner runner = new Runner();
         runner.runner();
-    }
 
-
-
-
-    private static void openFile() {
-        try {
-//constructor of file class having file as argument
-            File file = new File("src/main/resources/results.txt");
-            if (!Desktop.isDesktopSupported())//check if Desktop is supported by Platform or not
-            {
-                System.out.println("not supported");
-                return;
-            }
-            Desktop desktop = Desktop.getDesktop();
-            if (file.exists())         //checks file exists or not
-                desktop.open(file);              //opens the specified file
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
 
